f4b8a02ae3c43e95789bcd3f28bfa507046737c3
zchwang <73021537+zchwang@users.noreply.github.com>
2021-01-06 22:29:53 +0800

    Update prepare_PDB_file.sh

--- tutorials/samples/prepare_complexes/prepare_PDB_file.sh
+++ tutorials/samples/prepare_complexes/prepare_PDB_file.sh
@@ -3,20 +3,22 @@
 # Prepare the PDB file containing the 3D structure of protein-ligand complexes.
 
 cd $1
-dir_=$(ls $1)
+dir_=$(ls)
 
 for i in $dir_
 do
-	if [ `echo $i |wc -L` -eq 4 ];then
+	len=`expr length ${i}`
+	if [ -d $i ] && [ ${len} -eq 4 ];then
 		
 		cd $i
 		echo $i
 		
-		if [ ! -f ${i}_ligand.pdb ];then
+		if [ ! -e ${i}_ligand.pdb ];then
 			obabel ${i}_ligand.mol2 -O ${i}_ligand.pdb
+			echo "${i}_ligand.pdb generated ..."
 		fi
 		
-		if [ ! -f ${i}_ligand_renamed.pdb ] || [ ! -f ${i}_protein_atom.pdb ];then
+		if [ ! -e ${i}_ligand_renamed.pdb ] || [ ! -e ${i}_protein_atom.pdb ];then
 			python ../../prepare/change_name.py ${i}
 		fi
 		
